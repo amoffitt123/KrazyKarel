f596da49015b39d6d0f0ab9b50b3a2003e0a4fff
aaryand4420 <50377130+aaryand4420@users.noreply.github.com>
2019-05-17 13:20:38 -0400

    Update KarelBlocker.java

--- KarelBlocker.java
+++ KarelBlocker.java
@@ -4,13 +4,21 @@ import java.lang.Math;
 
 public class KarelBlocker extends Robot
 {
-   public KarelBlocker(int x)
+   public KarelBlocker(int x, KarelBlocker a)
    {
       super(10,x,Display.WEST,0);
+      for(int i = 0; i < 9; i++)
+         a.forward(a); 
    }
    public void collision(KarelBlocker a)
    {
       if(a.nextToARobot())
+         a.explode();
+   }
+   public void forward(KarelBlocker a)
+   {
+      a.move();
+      if(a.getX() < 1)
          a.explode();
    }
 }
